1df3ea781b69169d49abccfd9952110f73fda706
l00tus <marioalexandruu04@gmail.com>
2025-03-03 23:11:24 +0200

    add separate dockerfiles + deploy stuff

--- Dockerfile
+++ Dockerfile
@@ -7,31 +7,14 @@ COPY backend/package*.json ./
 RUN npm install
 COPY backend ./
 
-#Stage2 : build the frontend
-FROM node:22-alpine AS frontend-build
-
-WORKDIR /frontend
-
-COPY frontend/package*.json ./
-RUN npm install
-COPY frontend ./
-
-#Stage3 : combine both backend and frontend
+#Stage2 : run the backend
 FROM node:22-alpine
 
 WORKDIR /app
 
-#backend
 COPY --from=backend-build /backend /app
-
-#frontend
-COPY --from=frontend-build /frontend /app/public
-
+COPY .env /app/.env
  
-RUN npm install -g @angular/cli
-RUN echo 'cd /app && node index.js & cd /app/public && ng serve --port 4200 --host 0.0.0.0' > /start-apps.sh 
-RUN chmod +x /start-apps.sh
+EXPOSE 3000
 
-EXPOSE 4200
-
-CMD [ "sh", "-c", "/start-apps.sh"]
+CMD ["node", "index.js"]
--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -6,28 +6,29 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AppService {
+  private apiUrl = `https://friendr-service-869379210339.europe-west1.run.app`;
 
   constructor(private http: HttpClient) { }
 
   getUserByUsername(username: string): Observable<any> {
-    return this.http.get(`http://localhost:3000/users/${username}`);
+    return this.http.get(`${this.apiUrl}/users/${username}`);
   }
 
   createUser(firstName: string, lastName: string, username: string): Observable<any> {
     const userObject = { firstName, lastName, username };
-    return this.http.post('http://localhost:3000/users', userObject);
+    return this.http.post(`${this.apiUrl}/users`, userObject);
   }
 
   getPosts(): Observable<any>{
-    return this.http.get(`http://localhost:3000/posts`);
+    return this.http.get(`${this.apiUrl}/posts`);
   }
 
   createPost(author: string, title: string, description: string): Observable<any> {
     const postObject = { author, title, description };
-    return this.http.post(`http://localhost:3000/posts`, postObject);
+    return this.http.post(`${this.apiUrl}/posts`, postObject);
   }
 
   updatePostLikes(postId: string, username: string): Observable<any> {
-    return this.http.patch(`http://localhost:3000/posts/${postId}/likes`, { username });
+    return this.http.patch(`${this.apiUrl}/posts/${postId}/likes`, { username });
   }
 }
